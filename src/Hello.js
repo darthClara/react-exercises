cee040528915292fb02a0e90a96adfd9ebc84546
Clara Schillaci <dhdjjk>
2022-10-18 16:54:24 +0200

    Hello world fixed

--- src/Hello.js
+++ src/Hello.js
@@ -3,10 +3,7 @@ import React from "react";
 export default class Hello extends React.Component {
     render() {
         return (
-            <>
-                <h1>Hello, World!</h1>
-                <p>What a beautiful day!</p>
-            </>
+                <h1>Hello, World!</h1>  
         )
     }
 }
