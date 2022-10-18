c9a4980d98aba091504b3951fca7a9c471982de0
Clara Schillaci <dhdjjk>
2022-10-18 17:40:20 +0200

    Welcome commit

--- src/Hello.js
+++ src/Hello.js
@@ -3,10 +3,10 @@ import React from "react";
 export default class Hello extends React.Component {
     render() {
         return (
-            <>
+            <div>
                 <h1>Hello, World!</h1>
                 <p>What a beautiful day!</p>
-            </>
+            </div>
         )
     }
 }
