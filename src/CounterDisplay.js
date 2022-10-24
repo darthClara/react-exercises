4fcffd4d20261063497971d77bce3fdf7b4326a7
Clara Schillaci <dhdjjk>
2022-10-24 22:36:17 +0200

    events 01

--- src/CounterDisplay.js
+++ src/CounterDisplay.js
@@ -4,7 +4,7 @@ export default class CounterDisplay extends React.Component {
     render() {
         return (
             <h1>
-                Counter: {this.props.numero}
+                Automatic counter: {this.props.numero}
             </h1>
         )
     }
