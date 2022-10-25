32154520d13111e2dfd27352948f5303d49dcb53
Clara Schillaci <dhdjjk>
2022-10-25 13:51:15 +0200

    fixed events-01

--- src/App.js
+++ src/App.js
@@ -2,14 +2,22 @@ import React from "react";
 import Hello from "./Hello";
 import Welcome from "./Welcome";
 import Counter from "./Counter";
+import ClickCounter from "./FormsListsAndStyling/ClickCounter";
 
 export default class App extends React.Component {
     render() {
         return (
             <div>
+                <hr/>
+                <h2>Basic concepts</h2>
+                <hr/>
                 <Hello/>
                 <Welcome name="John" age={15}/>
                 <Counter numeroIniziale={0} intervallo={1000} incremento={1}/>
+                <hr/>
+                <h2>Forms, lists, and styling</h2>
+                <hr/>
+                <ClickCounter/>
             </div>
         )
     }
