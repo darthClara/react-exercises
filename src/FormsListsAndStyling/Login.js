d7e8489ae0797d809946764eee3202640f9133b1
Clara Schillaci <dhdjjk>
2022-10-27 23:16:14 +0200

    login

--- src/FormsListsAndStyling/Login.js
+++ src/FormsListsAndStyling/Login.js
@@ -3,23 +3,29 @@ import React from "react";
 export default class Login extends React.Component {
 
     state = {
-        remember: true,
+        remember: "false",
         pass: "",
         username: "",
+    }
+
+    handleClick = () => {
+        if (this.state.pass && this.state.username) {
+            this.props.onLogin(this.state);
+        } else {console.log("Inserisci il campo username e password")}
     }
 
     handleChange = (e) => {
         const value = e.target.value;
         const name = e.target.name;
 
-        if (value === true) {
+        if (value === "true") {
             this.setState({
-                remember: false
+                remember: "false"
             })
         }
-        else if (value === false) {
+        else if (value === "false") {
             this.setState({
-                remember: true
+                remember: "true"
             })
         }
         else if (name === "username") {
@@ -32,17 +38,23 @@ export default class Login extends React.Component {
                 pass: value
             })
         }
-
     }
+
+   
 
     render() {
         return (
             <div style={{ border: "1px solid black", margin: "20px 0px 20px 0px" }}>
-                <input type="text" name="username" value={this.state.value} />
+                <input type="text" name="username" value={this.state.value} onChange={this.handleChange}/>
+                <span> Username</span>
                 <br />
-                <input type="password" name="pass" value={this.state.value} />
+                <input type="password" name="pass" value={this.state.value} onChange={this.handleChange}/>
+                <span> Password</span>
                 <br />
-                <input type="checkbox" name="remember" value={this.state.remember} />
+                <input type="checkbox" name="remember" value={this.state.remember} onChange={this.handleChange}/>
+                <span> Remember</span>
+                <br />
+                <input type="button" name="login" value="LOGIN" onClick={this.handleClick}/>
             </div>
         )
     }
