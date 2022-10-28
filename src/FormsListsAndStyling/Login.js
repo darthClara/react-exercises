0e2f24cb90b6abfae0d57ca39f3732a36a6a0965
Clara Schillaci <dhdjjk>
2022-10-28 20:04:30 +0200

    reset button

--- src/FormsListsAndStyling/Login.js
+++ src/FormsListsAndStyling/Login.js
@@ -9,6 +9,7 @@ export default class Login extends React.Component {
     }
 
     handleClick = () => {
+
         if (this.state.pass && this.state.username) {
             this.onLogin(this.state);
         } else {console.log("Inserisci il campo username e password")}
@@ -19,6 +20,7 @@ export default class Login extends React.Component {
     }
 
     handleChange = (e) => {
+
         const value = e.target.value;
         const name = e.target.name;
 
@@ -46,18 +48,19 @@ export default class Login extends React.Component {
 
     render() {
         return (
-            <div style={{ border: "1px solid black", margin: "20px 0px 20px 0px" }}>
-                <input type="text" name="username" value={this.state.value} onChange={this.handleChange}/>
+            <form style={{ border: "1px solid black", margin: "20px 0px 20px 0px" }}>
+                <input type="text" className="formsElements" name="username" value={this.state.value} onChange={this.handleChange}/>
                 <span> Username</span>
                 <br />
-                <input type="password" name="pass" value={this.state.value} onChange={this.handleChange}/>
+                <input type="password" className="formsElements" name="pass" value={this.state.value} onChange={this.handleChange}/>
                 <span> Password</span>
                 <br />
-                <input type="checkbox" name="remember" value={this.state.remember} onChange={this.handleChange}/>
+                <input type="checkbox" className="formsElements" name="remember" value={this.state.remember} onChange={this.handleChange}/>
                 <span> Remember</span>
                 <br />
                 <input type="button" name="login" value="LOGIN" onClick={this.handleClick}/>
-            </div>
+                <input type="reset" name="reset" value="RESET" onClick={this.handleChange}/>
+            </form>
         )
     }
 }
