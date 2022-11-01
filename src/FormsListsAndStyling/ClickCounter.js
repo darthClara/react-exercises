68507e365c58aac374a4fe07896d708341eed9de
Clara Schillaci <dhdjjk>
2022-11-01 17:33:57 +0100

    ok2

--- src/FormsListsAndStyling/ClickCounter.js
+++ src/FormsListsAndStyling/ClickCounter.js
@@ -1,23 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default class ClickCounter extends React.Component {
+// export default class ClickCounter extends React.Component {
 
-    state = {
-        numero: 0
+//     state = {
+//         numero: 0
+//     }
+
+//     handleClick = () => {
+//         this.setState({
+//             numero: this.state.numero + 1
+//         })
+//     }
+
+//     render() {
+//         return (
+//             <div>
+//                 <h1>Clicker counter: {this.state.numero}</h1>
+//                 <button onClick={this.handleClick}>INCREMENTA</button>
+//             </div>
+//         )
+//     }
+// }
+
+export default function ClickCounter() {
+
+    // state = {
+    //     numero: 0
+    // }
+
+    // handleClick = () => {
+    //     setState({
+    //         numero: state.numero + 1
+    //     })
+    // }
+
+    const [numero, setNumero] = useState(0);
+
+    function handleClick() {
+        setNumero(numero + 1)
     }
 
-    handleClick = () => {
-        this.setState({
-            numero: this.state.numero + 1
-        })
-    }
-
-    render() {
         return (
             <div>
-                <h1>Clicker counter: {this.state.numero}</h1>
-                <button onClick={this.handleClick}>INCREMENTA</button>
+                <h1>Clicker counter: {numero}</h1>
+                <button onClick={handleClick}>INCREMENTA</button>
             </div>
         )
-    }
 }
