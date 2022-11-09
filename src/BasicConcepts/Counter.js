2d666844b293e627744916cfe1b2893d54938bd7
Clara Schillaci <dhdjjk>
2022-11-09 12:37:34 +0100

    02

--- src/BasicConcepts/Counter.js
+++ src/BasicConcepts/Counter.js
@@ -1,31 +1,56 @@
-import React from "react";
-import CounterDisplay from "./CounterDisplay";
+// import React from "react";
+// import CounterDisplay from "./CounterDisplay";
 
-export default class Counter extends React.Component {
+// export default class Counter extends React.Component {
     
-    state = {
-        numero: this.props.numeroIniziale,
-    };
+//     state = {
+//         numero: this.props.numeroIniziale,
+//     };
 
-    componentDidMount() {
-        this.timer = setInterval(
-          () => this.setState({
-            numero: this.state.numero + this.props.incremento,
-        }),
-          this.props.intervallo
-        );
-      }
+//     componentDidMount() {
+//         this.timer = setInterval(
+//           () => this.setState({
+//             numero: this.state.numero + this.props.incremento,
+//         }),
+//           this.props.intervallo
+//         );
+//       }
 
-      componentWillUnmount() {
-        clearInterval(this.timer);
-      }
+//       componentWillUnmount() {
+//         clearInterval(this.timer);
+//       }
 
 
-    render() {
-        return (
-            <div>
-                <CounterDisplay numero={this.state.numero}/>
-            </div>
-        )
-    }
+//     render() {
+//         return (
+//             <div>
+//                 <CounterDisplay numero={this.state.numero}/>
+//             </div>
+//         )
+//     }
+// }
+
+
+import CounterDisplay from "./CounterDisplay";
+import React, { useEffect, useState } from "react";
+
+export default function Counter() {
+
+const [ counter, setCounter ] = useState(0)
+
+useEffect(() => {
+    const clear = setInterval(() => {
+        setCounter(counter + 1)
+    }, 1000);
+    return () => clearInterval(clear)
+}, [counter])
+
+
+
+
+  return (
+    <div>
+        <CounterDisplay numero={counter}/>
+    </div>
+  )
 }
