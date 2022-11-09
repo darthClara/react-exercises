2984fbfc4c8a365ae30db17028ad2b21f8bdd088
Clara Schillaci <dhdjjk>
2022-11-09 13:22:17 +0100

    ok

---
+++ src/FunctionComponentsAndHooks/GithubUserList.js
@@ -0,0 +1,34 @@
+import React, {useEffect, useState} from "react"
+import { GithubUser } from "./GithubUser"
+
+export default function GithubUserList() {
+    const [input, setInput] = useState("")
+    const [array, setArray] = useState([])
+
+    function handleInput(e) {
+        const value = e.target.value
+        setInput(value)
+    }
+
+    function handleClick() {
+        setArray([...array, input])
+    }
+
+    useEffect(() => {
+
+    }, [array])
+
+  return (
+    <div>
+      <div>
+        <input value={input} onChange={handleInput}></input>
+        <button onClick={handleClick}>TROVA L'UTENTE</button>
+      </div>
+      <div>
+        {Array.map((user, index) => (
+            <GithubUser username={user} key={index}/>
+        ))}
+      </div>
+    </div>
+  )
+}
