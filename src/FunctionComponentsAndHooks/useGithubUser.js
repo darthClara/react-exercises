d26bf25576aec74e4dcde03b4112f45cbb5ba317
Clara Schillaci <dhdjjk>
2022-11-14 15:26:33 +0100

    ok

--- src/FunctionComponentsAndHooks/useGithubUser.js
+++ src/FunctionComponentsAndHooks/useGithubUser.js
@@ -2,15 +2,25 @@ import React, {useState, useEffect} from "react"
 
 export default function useGithubUser(name) {
     const [data, setData] = useState(null)
+    const [loading, setLoading] = useState(false)
+    const [errore, setErrore] = useState(null)
 
 
     async function fetchData(name) {
 
-        const response =  await fetch(`https://api.github.com/users/${name}`)
+        setLoading(true)
+        setErrore(null)
+        try {
+            const response =  await fetch(`https://api.github.com/users/${name}`)
         const json = await response.json()
         setData(json)
+        } catch (err) {
+            setErrore(err)
+            setData(null)
+        } finally {
+            setLoading(false)
         }
-
+        }
 
     useEffect(() => {
 
@@ -19,6 +29,6 @@ export default function useGithubUser(name) {
     }, [name])
 
     return {
-        data
+        data, loading, errore
     }
 }
